f5e0a8a270aa4900b31b42306b8b0d1e71780a54
Samitha Wickramasinghe <contact.samitha@gmail.com>
2025-04-17 14:56:09 +0530

    fix issues

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const destinationRoutes = require("./routes/destination.route");
 
 // const mongoDB = "mongodb+srv://root:root@cluster0.spmcifx.mongodb.net/ctse";
-const mongoDB = "mongodb+srv://root:root@cluster0.u7ficrd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const mongoDB = process.env.MONGODB_URI;
 
 // Connect to MongoDB
 mongoose
